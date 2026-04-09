8d03829cca44dd6c42e8d2b512afedc11fd19ff6
KKamtte <swye199@gmail.com>
2026-04-09 16:22:09 +0900

    chore: 기본생성자 제한

--- external/naver-client/src/main/java/library/NaverErrorResponse.java
+++ external/naver-client/src/main/java/library/NaverErrorResponse.java
@@ -1,5 +1,13 @@
 package library;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NaverErrorResponse {
     private String errorMessage;
     private String errorCode;
@@ -7,9 +15,5 @@ public class NaverErrorResponse {
     public NaverErrorResponse(String errorMessage, String errorCode) {
         this.errorMessage = errorMessage;
         this.errorCode = errorCode;
-    }
-
-    public String getErrorMessage() {
-        return errorMessage;
     }
 }
